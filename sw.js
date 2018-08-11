79971951b82d8292fa92ddea481460d9a4ec5edd
bright-ic <40720995+bright-ic@users.noreply.github.com>
2018-08-11 09:41:31 +0100

    Update sw.js
    Added restaurant pictures as static files to be pre-cached when service worker is installed

--- sw.js
+++ sw.js
@@ -6,7 +6,17 @@ const filesToCache = [
     "/data/restaurants.json",
     "/js/dbhelper.js",
     "/js/main.js",
-    "/js/restaurant_info.js"
+    "/js/restaurant_info.js",
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/4.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+    "/img/7.jpg",
+    "/img/8.jpg",
+    "/img/9.jpg",
+    "/img/10.jpg"
 ];
 const appCacheName = "resturant-review-app-cache-v1";
 
