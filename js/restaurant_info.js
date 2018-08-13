78fff13cfd9ffbd0736acc185aac37c8a6c7a58e
bright-ic <40720995+bright-ic@users.noreply.github.com>
2018-08-13 13:26:39 +0100

    Update on restaurant_info.js
    Adding accessibility [focus ] to restaurant review li tags

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -131,7 +131,8 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
+  title.tabIndex = 0;
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
@@ -153,6 +154,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  li.tabIndex = 0;
   const name = document.createElement('p');
   name.innerHTML = review.name;
   li.appendChild(name);
